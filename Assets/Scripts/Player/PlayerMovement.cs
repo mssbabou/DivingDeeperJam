f90bea6671ee07b43009bbc0391c9fa2d91efe1f
Christoffer Hald Christensen <chrishald@outlook.dk>
2023-08-20 18:25:01 +0200

    In PlayerMovement: Removed debugging

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -21,9 +21,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("movement input: " + playerInput.movementInput);
-        Debug.Log("Movement applied: " + Time.deltaTime * acceleration * playerInput.movementInput);
-
         MovePlayer();
     }
 
