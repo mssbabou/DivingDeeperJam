da9796f02718f8872838ae8d77f019b5eee4b6c7
Christoffer Hald Christensen <chrishald@outlook.dk>
2023-08-20 18:14:23 +0200

    Enemy moves toward Submarine

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform target;
+
+    [Header("Stats")]
+    [SerializeField] EnemyStats stats;
+
+
+    Rigidbody2D rb;
+    Vector2 direction;
+
+    void Awake ()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction = (target.position - transform.position).normalized;
+        MoveToTarget();
+    }
+
+    void MoveToTarget()
+    {
+        rb.AddForce(direction, ForceMode2D.Force);
     }
 }
