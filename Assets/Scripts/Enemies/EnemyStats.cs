31934f2aed0e89c79c4b2e0d5f0a7e46b49e9611
Christoffer Hald Christensen <chrishald@outlook.dk>
2023-08-20 18:30:09 +0200

    Made an enemyType enum

--- Assets/Scripts/Enemies/EnemyStats.cs
+++ Assets/Scripts/Enemies/EnemyStats.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemyStats", menuName = "ScriptableObjects/EnemyStats", order = 1)]
 public class EnemyStats : ScriptableObject
 {
+    [Header("Type")]
+    public EnemyType type;
+
     [Header("Defensive")]
     public float health;
     public float armor;
@@ -15,4 +18,12 @@ public class EnemyStats : ScriptableObject
 
     [Header("Mobility")]
     public float speed;
+}
+
+public enum EnemyType
+{
+    melee,
+    ranged,
+    suicide,
+    tank
 }
